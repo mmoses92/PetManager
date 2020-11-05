be07fcafe3e9f4069427d946e800a14f0e7fb56d
max.moses <max.moses@my.uwrf.edu>
2020-11-05 11:35:34 -0600

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Driver {
+
+    public static void main(String[] args){
+        
+        GUI myPet = new GUI();
+
+    }
+
+}
---
+++ src/GUI.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame{
+
+    //private JLabel length, width, area, perimeter;
+
+    public GUI() {
+        setTitle("My Pet Manager");
+
+        //Container pane = getContentPane();
+        //pane.setLayout(new GridLayout(4,1));
+
+        setSize(500, 500);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+}
